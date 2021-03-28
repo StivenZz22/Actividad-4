2e29a19f3fc57e658d63d5f86307590864b81af8
StivenZz22 <salinasosorio1234@gmail.com>
2021-03-27 23:34:04 -0500

    Actividad 4 git

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function mensaje(){
+    alert("Estas seguro de que deseas visitar este sitio ?")
+}
+
+
+
+
+
+$("#final").click(function(){
+$(this).css("font-family","cursive")
+
+})
+
+
+
+$("#color").click(function(){
+   $(this).css("background-color","brown")
+})
+
+$("#inhibidor").dblclick(function(){
+    
+ $(this).css('font-size','30px')
+
+
+})
